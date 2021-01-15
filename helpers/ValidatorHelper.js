75749b7f4d3e06379609b988cc43cb589dc32bd3
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-01-15 17:48:28 +0200

    Create validation for editing media

--- helpers/ValidatorHelper.js
+++ helpers/ValidatorHelper.js
@@ -2,47 +2,47 @@ import Joi from 'joi';
 import { ConstantHelper } from './ConstantHelper';
 
 export class ValidatorHelper extends ConstantHelper {
-  constructor(data) {
-    super();
-    this.data = data;
-  }
+	constructor(data) {
+		super();
+		this.data = data;
+	}
 
-  validateInput(type) {
-    let validateKeys = null;
-    switch (type) {
-      case 'login':
-        validateKeys = Joi.object().keys(this.getLoginKeys());
-        break;
-      case 'user':
-        validateKeys = Joi.object().keys(this.getUserKeys());
-        break;
-      case 'newTopic':
-        validateKeys = Joi.object().keys(this.getTopicKeys());
-        break;
-      case 'existingTopic':
-        validateKeys = Joi.object().keys(this.existingTopicKeys());
-        break;
-      case 'category':
-        validateKeys = Joi.object().keys(this.categoryKeys());
-        break;
-      case 'album':
-        validateKeys = Joi.object().keys(this.albumKeys());
-        break;
-      case 'media':
-        validateKeys = Joi.object().keys(this.mediaKeys());
-        break;
-      case 'announcemt':
-        validateKeys = Joi.object().keys(this.announcemtKeys());
-        break;
-      case 'comment':
-        validateKeys = Joi.object().keys(this.commentKeys());
-        break;
-      case 'message':
-        validateKeys = Joi.object().keys(this.messageKeys());
-        break;
-      default:
-        break;
-    }
-    return Joi.validate(this.data, validateKeys);
-  }
+	validateInput(type) {
+		let validateKeys = null;
+		switch (type) {
+			case 'login':
+				validateKeys = Joi.object().keys(this.getLoginKeys());
+				break;
+			case 'user':
+				validateKeys = Joi.object().keys(this.getUserKeys());
+				break;
+			case 'newTopic':
+				validateKeys = Joi.object().keys(this.getTopicKeys());
+				break;
+			case 'existingTopic':
+				validateKeys = Joi.object().keys(this.existingTopicKeys());
+				break;
+			case 'category':
+				validateKeys = Joi.object().keys(this.categoryKeys());
+				break;
+			case 'album':
+				validateKeys = Joi.object().keys(this.albumKeys());
+				break;
+			case 'media':
+				validateKeys = Joi.object().keys(this.mediaKeys());
+				break;
+			case 'announcemt':
+				validateKeys = Joi.object().keys(this.announcemtKeys());
+				break;
+			case 'comment':
+				validateKeys = Joi.object().keys(this.commentKeys());
+				break;
+			case 'message':
+				validateKeys = Joi.object().keys(this.messageKeys());
+				break;
+			default:
+				break;
+		}
+		return Joi.validate(this.data, validateKeys);
+	}
 }
