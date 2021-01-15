50c4f02e4eb698f9e65aee3692582aea94c55544
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-01-15 17:47:33 +0200

    Create migration for adding author and actionDate in media table

---
+++ migrations/20210115141130-add_media_columns_author_date.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return Promise.all(
+			[
+				queryInterface.addColumn('media', 'author', {
+					type: Sequelize.STRING,
+					allowNull: true
+				})
+			],
+			[
+				queryInterface.addColumn('media', 'actionDate', {
+					type: Sequelize.DATE,
+					allowNull: true
+				})
+			]
+		);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		return Promise.all([
+			queryInterface.removeColumn('media', 'author'),
+			queryInterface.removeColumn('media', 'actionDate')
+		]);
+	}
+};
