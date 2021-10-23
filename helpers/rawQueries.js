ba9e971a7e669226c6e8982121ffa891d0d772e4
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-10-23 14:04:22 +0200

    Change column as integer

--- helpers/rawQueries.js
+++ helpers/rawQueries.js
@@ -12,14 +12,14 @@ group by c.id, c."name", c.slug
 export const topicViewsQuery = (languageId) => {
   return `
   select 
-      t.id, t.title, t."coverImage", t."content", t_v.topic_views
+      t.id, t.title, t."coverImage", t."content", cast(t_v.topic_views as int)
     from topics t
     inner join (
       select "topicId", count(tv2."topicId") as topic_views 
       from topic_views tv2 group by tv2."topicId"
     ) as t_v on t.id = t_v."topicId"
     where t."languageId"=${languageId} 
-    order by t_v.topic_views asc
+    order by t_v.topic_views desc
     limit 4;
   `;
 };
