c8af48445c8e2bab0d82d4b2d0afb0c0547e0128
Akimana A Jean d Amour <akimanaja17@gmail.com>
2021-12-27 09:45:21 +0200

    Fix special characters for file names

--- helpers/fileHelper.js
+++ helpers/fileHelper.js
@@ -1,88 +1,88 @@
-import { existsSync, mkdirSync, unlink } from 'fs';
-import multer from 'multer';
-import path from 'path';
-import { Topic } from '../models';
-import { ConstantHelper } from './ConstantHelper';
-import { QueryHelper } from './QueryHelper';
-import { ACCEPTED_FILE_SIZE, isFileAllowed, serverResponse } from './util';
+import { existsSync, mkdirSync, unlink } from "fs";
+import multer from "multer";
+import path from "path";
+import { Topic } from "../models";
+import { ConstantHelper } from "./ConstantHelper";
+import { QueryHelper } from "./QueryHelper";
+import { ACCEPTED_FILE_SIZE, isFileAllowed, serverResponse } from "./util";
 
 const dbTopicHelper = new QueryHelper(Topic);
 const storage = multer.diskStorage({
-	destination: (req, file, callBack) => {
-		let fileStorage = null;
-		const { path } = req;
-		if (path.includes('upload/image')) fileStorage = process.env.IMAGES_ZONE;
-		else if (path.includes('upload/song')) fileStorage = process.env.SONGS_ZONE;
-		else callBack(ConstantHelper.serverError);
-		callBack(null, fileStorage);
-	},
-	filename: (req, file, callBack) => {
-		let ext = path.extname(file.originalname).split('.')[1];
-		let fileName = file.originalname.split('.')[0];
-		let mediaLink = `${fileName}-${Date.now()}.${ext}`;
-		callBack(null, mediaLink);
-	}
+  destination: (req, file, callBack) => {
+    let fileStorage = null;
+    const { path } = req;
+    if (path.includes("upload/image")) fileStorage = process.env.IMAGES_ZONE;
+    else if (path.includes("upload/song")) fileStorage = process.env.SONGS_ZONE;
+    else callBack(ConstantHelper.serverError);
+    callBack(null, fileStorage);
+  },
+  filename: (req, file, callBack) => {
+    let ext = path.extname(file.originalname).split(".")[1];
+    let fileName = file.originalname.split(".")[0];
+    let mediaLink = `${fileName}-${Date.now()}.${ext}`;
+    callBack(null, mediaLink);
+  },
 });
 
 export const uploadSingle = multer({
-	storage
-}).single('file');
-export const uploadMany = multer({ storage }).array('file');
+  storage,
+}).single("file");
+export const uploadMany = multer({ storage }).array("file");
 
 export const uploadSingleFile = async (req, res) => {
-	let fileStorage = null;
-	const { fileType } = req.params;
-	const { prevFile, update } = req.query;
-	if (fileType === 'image') {
-		fileStorage = process.env.IMAGES_ZONE;
-	} else if (fileType === 'song') {
-		fileStorage = process.env.SONGS_ZONE;
-	} else return serverResponse(res, 400, 'Unknown file upload');
-	/**
-	 * Delete the previous file if exist
-	 */
-	if (!existsSync(fileStorage)) {
-		mkdirSync(fileStorage, { recursive: true });
-	}
-	if (prevFile) {
-		unlink(`${fileStorage}/${prevFile}`, () => {});
-	}
-	const storage = multer.diskStorage({
-		destination: (req, file, callBack) => {
-			callBack(null, fileStorage);
-		},
-		filename: (req, file, callBack) => {
-			let ext = path.extname(file.originalname).split('.')[1];
-			let fileName = file.originalname.split('.')[0];
-			let mediaLink = `${fileName}-${Date.now()}.${ext}`;
-			callBack(null, mediaLink);
-		}
-	});
-	const upload = multer({
-		storage,
-		limits: { fileSize: ACCEPTED_FILE_SIZE },
-		fileFilter: (req, file, filterCallBack) => {
-			isFileAllowed(file, fileStorage, (error, allowed) => {
-				if (error) return filterCallBack(error);
-				return filterCallBack(null, allowed);
-			});
-		}
-	}).single('file');
+  let fileStorage = null;
+  const { fileType } = req.params;
+  const { prevFile, update } = req.query;
+  if (fileType === "image") {
+    fileStorage = process.env.IMAGES_ZONE;
+  } else if (fileType === "song") {
+    fileStorage = process.env.SONGS_ZONE;
+  } else return serverResponse(res, 400, "Unknown file upload");
+  /**
+   * Delete the previous file if exist
+   */
+  if (!existsSync(fileStorage)) {
+    mkdirSync(fileStorage, { recursive: true });
+  }
+  if (prevFile) {
+    unlink(`${fileStorage}/${prevFile}`, () => {});
+  }
+  const storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+      callBack(null, fileStorage);
+    },
+    filename: (req, file, callBack) => {
+      let ext = path.extname(file.originalname).split(".")[1];
+      let fileName = file.originalname.split(".")[0].replace(/\W/g, " ").trim();
+      let mediaLink = `${fileName}-${Date.now()}.${ext}`;
+      callBack(null, mediaLink);
+    },
+  });
+  const upload = multer({
+    storage,
+    limits: { fileSize: ACCEPTED_FILE_SIZE },
+    fileFilter: (req, file, filterCallBack) => {
+      isFileAllowed(file, fileStorage, (error, allowed) => {
+        if (error) return filterCallBack(error);
+        return filterCallBack(null, allowed);
+      });
+    },
+  }).single("file");
 
-	upload(req, res, async (uploadError) => {
-		if (uploadError instanceof multer.MulterError || uploadError) {
-			const errorMsg = uploadError.message || uploadError;
-			return serverResponse(res, 500, errorMsg);
-		}
+  upload(req, res, async (uploadError) => {
+    if (uploadError instanceof multer.MulterError || uploadError) {
+      const errorMsg = uploadError.message || uploadError;
+      return serverResponse(res, 500, errorMsg);
+    }
 
-		if (!req.file) return serverResponse(res, 400, 'No file selected');
-		const fileName = req.file.filename;
-		if (fileType === 'image' && update !== 'null') {
-			await dbTopicHelper.update(
-				{ coverImage: fileName },
-				{ coverImage: prevFile }
-			);
-		}
-		return serverResponse(res, 200, 'File(s) uploaded', fileName);
-	});
+    if (!req.file) return serverResponse(res, 400, "No file selected");
+    const fileName = req.file.filename;
+    if (fileType === "image" && update !== "null") {
+      await dbTopicHelper.update(
+        { coverImage: fileName },
+        { coverImage: prevFile }
+      );
+    }
+    return serverResponse(res, 200, "File(s) uploaded", fileName);
+  });
 };
