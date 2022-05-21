402c96b79d7dabad9e6a1281a2ad95d5e14e57a1
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-05-21 21:42:04 +0200

    Add fix: retrive only published

--- src/helpers/rawQueries.js
+++ src/helpers/rawQueries.js
@@ -1,11 +1,17 @@
 export const categoriesTopicQuery = (languageId) => {
   return `
-select c.id, c."name", c.slug
-from topics t 
-inner join categories c 
-on t."categoryId" = c.id 
-where t."languageId"=${languageId} 
-group by c.id, c."name", c.slug
+    select 
+      c.id, c."name", c.slug
+    from 
+      topics t 
+    inner join 
+      categories c 
+    on 
+      t."categoryId" = c.id 
+    where 
+      t."languageId"=${languageId} 
+    group by 
+      c.id, c."name", c.slug
 `;
 };
 
@@ -18,7 +24,7 @@ export const topicViewsQuery = (languageId) => {
       select "topicId", count(tv2."topicId") as topic_views 
       from topic_views tv2 group by tv2."topicId"
     ) as t_v on t.id = t_v."topicId"
-    where t."languageId"=${languageId} 
+    where t."languageId"=${languageId} AND t."isPublished"=true
     order by t_v.topic_views desc
     limit 4;
   `;
