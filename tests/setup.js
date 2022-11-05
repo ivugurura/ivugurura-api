1995df34e2450bbdf19d4d15f5c87cdec8d5a708
Akimana A Jean d Amour <akimanaja17@gmail.com>
2022-11-05 23:13:23 +0200

    Set up tests

---
+++ tests/setup.js
@@ -0,0 +1,12 @@
+import chai from "chai";
+import { faker } from "@faker-js/faker";
+import chaiAsPromise from "chai-as-promised";
+
+chai.use(chaiAsPromise);
+
+export const expect = chai.expect;
+export const chatRoomUser = {
+  socketId: faker.datatype.uuid(),
+  userId: faker.datatype.uuid(),
+  name: "test-user",
+};
