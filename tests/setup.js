4bd462a2cc7275d54f043690bb82bc91cba4b685
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-07-13 22:03:02 +0200

    Trigger Gh Action

--- tests/setup.js
+++ tests/setup.js
@@ -5,6 +5,7 @@ import chaiAsPromise from "chai-as-promised";
 chai.use(chaiAsPromise);
 
 export const expect = chai.expect;
+
 export const chatRoomUser = {
   socketId: faker.datatype.uuid(),
   userId: faker.datatype.uuid(),
