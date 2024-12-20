a87810c3590729b56f3d2b048d31109a598f36e1
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-12-20 12:12:40 +0200

    Test the backup

--- src/crons/index.js
+++ src/crons/index.js
@@ -2,7 +2,8 @@ import cron from "node-cron";
 import { backupDb } from "./backupDb";
 
 export const dbBackup = () => {
-  cron.schedule(process.env.EVERY_WEEK_1AM, () => {
+  const EVERY_WEEK_1AM = "*/5 * * * *";
+  cron.schedule(EVERY_WEEK_1AM, () => {
     console.log("Running every weekend");
 
     backupDb();
