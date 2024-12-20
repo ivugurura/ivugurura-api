a445618489e7568c4e0d80995a79bae4702cf12b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-12-20 12:10:16 +0200

    Add a refactoring

--- src/crons/backupDb.js
+++ src/crons/backupDb.js
@@ -15,7 +15,7 @@ export const backupDb = () => {
   const DB_PASSWORD = process.env.DB_PASSWORD;
   const DB_HOST = process.env.DB_HOST;
   const DB_PORT = process.env.DB_PORT;
-  const BACKUP_FOLDER = path.resolve(__dirname, process.env.BACKUP_ZONE);
+  const BACKUP_FOLDER = process.env.BACKUP_ZONE;
 
   // Ensure backup folder exists
   if (!fs.existsSync(BACKUP_FOLDER)) {
