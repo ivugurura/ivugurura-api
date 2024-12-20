3ddeb18a27435bc04f4d34a11e44a7aae045c23b
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-12-20 12:47:17 +0200

    Remove tests

--- src/crons/index.js
+++ src/crons/index.js
@@ -2,8 +2,11 @@ import cron from "node-cron";
 import { backupDb } from "./backupDb";
 
 export const dbBackup = () => {
-  const EVERY_WEEK_1AM = "*/5 * * * *";
-  cron.schedule(EVERY_WEEK_1AM, () => {
+  /**
+   * Uncomment the following line when testing in local
+   */
+  // process.env.EVERY_WEEK_1AM = "* * * * *";
+  cron.schedule(process.env.EVERY_WEEK_1AM, () => {
     console.log("Running every weekend");
 
     backupDb();
