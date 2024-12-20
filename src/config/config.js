65193c2b1be0d0a34ce80316bfa75bd53569c9bc
Akimana A Jean d Amour <akimanaja17@gmail.com>
2024-12-20 12:02:27 +0200

    Add a config reefactoring

--- src/config/config.js
+++ src/config/config.js
@@ -3,37 +3,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // query: { raw: true, nest: true }
-const queryOptions = {};
+const queryOptions = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  seederStorage: "sequelize",
+};
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DEV_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    // dialectOptions: {
-    //   charset: 'utf8mb4',
-    //   collate: 'utf8mb4_unicode_ci'
-    // },
-    seederStorage: "sequelize",
-    ...queryOptions,
-  },
+  development: queryOptions,
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_TEST_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    dialectOptions: {
-      charset: "utf8mb4",
-    },
-    seederStorage: "sequelize",
     ...queryOptions,
+    database: process.env.DB_TEST_NAME,
   },
   production: {
     use_env_variable: "DATABASE_URL",
-    ...queryOptions,
   },
 };
