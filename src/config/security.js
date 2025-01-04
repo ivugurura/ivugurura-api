a8523e2444b68345ab5d49e6ecbdd3f4d9fbf056
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-01-04 18:50:09 +0200

    Configure allowed host

--- src/config/security.js
+++ src/config/security.js
@@ -1,13 +1,33 @@
-import helmet from 'helmet';
+import helmet from "helmet";
 //Here you can spoof any back end
-const SPOOFED_SERVER = 'Phusion Passenger (mod_rails/mod_rack) 3.0.11';
+const SPOOFED_SERVER = "Phusion Passenger (mod_rails/mod_rack) 3.0.11";
 
 export const security = (app) => {
   app
     .use(helmet({ contentSecurityPolicy: false }))
     .use(helmet.noSniff())
-    .use(helmet.frameguard({ action: 'deny' }))
+    .use(helmet.frameguard({ action: "deny" }))
     .use(helmet.xssFilter())
     .use(helmet.hidePoweredBy({ setTo: SPOOFED_SERVER }))
     .use(helmet.dnsPrefetchControl({ allow: false }));
+};
+
+// Define allowed origins
+const allowedOrigins = [
+  "http://localhost:3000", // Allow localhost
+  "https://reformationvoice.com",
+];
+export const corseOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      // Allow requests with no origin (like mobile apps or Postman)
+      callback(null, true);
+    } else {
+      // Reject requests from other origins
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // List allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // List allowed headers
+  credentials: true, // If you want to allow cookies and credentials
 };
