546386c364150e8fbd1a71d278aee7fc66537b1c
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-02-10 14:45:14 +0200

    Register routes and controlers

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { catchErrors } from "../middlewares";
+import { getBookCategories, getBooks, readBook } from "./controllers";
 
 const bookRoutes = Router();
 
-bookRoutes.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from books" });
-});
+bookRoutes.get("/", catchErrors(getBooks));
+bookRoutes.get("/categories", catchErrors(getBookCategories));
+bookRoutes.get("/:id", catchErrors(readBook));
+
+export default bookRoutes;
