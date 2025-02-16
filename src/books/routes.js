626ef3bf21d785d403078f7c03b5dd9a420f6e81
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-02-16 21:50:46 +0200

    Create book - Register a route

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { catchErrors } from "../middlewares";
-import { getBookCategories, getBooks, readBook } from "./controllers";
+import { catchErrors, isAdminOrEditor } from "../middlewares";
+import {
+  getBookCategories,
+  getBooks,
+  readBook,
+  registerBook,
+} from "./controllers";
+import { isBookValid } from "./middlewares";
 
 const bookRoutes = Router();
 
+bookRoutes.post("/", isAdminOrEditor, isBookValid, catchErrors(registerBook));
 bookRoutes.get("/", catchErrors(getBooks));
 bookRoutes.get("/categories", catchErrors(getBookCategories));
-bookRoutes.get("/:id", catchErrors(readBook));
+bookRoutes.get("/:bookId", catchErrors(readBook));
 
 export default bookRoutes;
