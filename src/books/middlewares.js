2323dc4f37c726a4493b9bde30d50f9ccb9f032f
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-06 10:54:46 +0200

    Clean the old validation

--- src/books/middlewares.js
+++
@@ -1,8 +0,0 @@
-import { joiValidatorMsg, ValidatorHelper } from "../helpers";
-
-export const isBookValid = (req, res, next) => {
-  let validator = new ValidatorHelper(req.body);
-  const errorBody = validator.validateInput("book");
-  if (errorBody.error) return joiValidatorMsg(res, errorBody);
-  return next();
-};
