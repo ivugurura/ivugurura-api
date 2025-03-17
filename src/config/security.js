2b6491aa4bbbee4edcba7e08ebcac1481f440200
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-17 19:51:07 +0200

    Config headers

--- src/config/security.js
+++ src/config/security.js
@@ -34,6 +34,11 @@ export const corseOptions = {
     }
   },
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Timestamp",
+    "X-Signature",
+  ],
   credentials: true,
 };
