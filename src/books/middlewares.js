876e8d4b025c34638771b01d5a4fe94830b8f877
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-03-17 19:50:44 +0200

    Configure restriction when downloading books

--- src/books/middlewares.js
+++ src/books/middlewares.js
@@ -1,3 +1,6 @@
+// import crypto from "crypto";
+import { authenticatedUser, serverResponse, systemRoles } from "../helpers";
+
 /**
  *
  * @param {*} req
@@ -5,23 +8,34 @@
  * @param {*} next
  * @returns
  */
-export const validateBookAccess = (req, res, next) => {
-  const apiKey = "your-secret-key";
-  const requestTimestamp = req.headers["x-timestamp"];
-  const requestHash = req.headers["x-signature"];
-
-  if (!requestTimestamp || !requestHash) {
-    return res.status(403).json({ error: "Missing authentication headers" });
+export const validateBookAccess = async (req, res, next) => {
+  const book = req.body.book;
+  if (book.isDownloadable) {
+    return next();
   }
 
-  const expectedHash = crypto
-    .createHmac("sha256", apiKey)
-    .update(requestTimestamp)
-    .digest("hex");
-
-  if (requestHash !== expectedHash) {
-    return res.status(403).json({ error: "Invalid signature" });
+  const user = await authenticatedUser(req);
+  if (book.userId === user?.id || user.role <= systemRoles.editor) {
+    return next();
   }
 
-  return next();
+  return serverResponse(res, 403, "You are not allowed to download this book");
+  // const apiKey = "your-secret-key";
+  // const requestTimestamp = req.headers["x-timestamp"];
+  // const requestHash = req.headers["x-signature"];
+
+  // if (!requestTimestamp || !requestHash) {
+  //   return res.status(403).json({ error: "Missing authentication headers" });
+  // }
+
+  // const expectedHash = crypto
+  //   .createHmac("sha256", apiKey)
+  //   .update(requestTimestamp)
+  //   .digest("hex");
+
+  // if (requestHash !== expectedHash) {
+  //   return res.status(403).json({ error: "Invalid signature" });
+  // }
+
+  // return next();
 };
